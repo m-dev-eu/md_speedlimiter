cfebb92af39d88bcb1b5a1fb522f190c12ffef11
Mirow <95571243+Mirrrrrow@users.noreply.github.com>
2024-01-29 13:54:59 +0100

    Add global speed limit override for vehicles

--- client.lua
+++ client.lua
@@ -2,10 +2,16 @@ if not lib then
     error('ox_lib required')
 end
 
+local GLOBAL_LIMIT = Vehicles['GLOBAL']
+
 lib.onCache('vehicle', function(value)
     if not value then return end
     SetTimeout(0, function()
         if cache.seat ~= -1 then return end
+
+        if GLOBAL_LIMIT then
+            return SetEntityMaxSpeed(value, GLOBAL_LIMIT / 3.6)
+        end
 
         local vehicleLimit = Vehicles[GetEntityModel(value)]
         if not vehicleLimit then return end
--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 Vehicles = {
+    ['GLOBAL'] = 200, -- IF THIS IS SET TO ANYTHING BUT NIL IT WILL OVERRIDE ALL OTHER LIMITS
     [GetHashKey('benson')] = 148,
     [GetHashKey('biff')] = 123,
     [GetHashKey('hauler')] = 163,
